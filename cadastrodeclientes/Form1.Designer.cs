faab03ed8dd39f9f5ffaddc68878f4dafa86107e
gustavorssilva3 <gustavo.rssilva3@senacsp.edu.br>
2025-04-03 17:16:43 -0300

    Aula 03-04

---
+++ cadastrodeclientes/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace cadastrodeclientes
+{
+    partial class frmCadastrodeClientes
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTopo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.tb = new System.Windows.Forms.TabControl();
+            this.tbCadastro = new System.Windows.Forms.TabPage();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNomeSocial = new System.Windows.Forms.TextBox();
+            this.lblNomeSocial = new System.Windows.Forms.Label();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.tbConsulta = new System.Windows.Forms.TabPage();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.panelTopo.SuspendLayout();
+            this.tb.SuspendLayout();
+            this.tbCadastro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTopo
+            // 
+            this.panelTopo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelTopo.Controls.Add(this.lblTitulo);
+            this.panelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopo.Location = new System.Drawing.Point(0, 0);
+            this.panelTopo.Name = "panelTopo";
+            this.panelTopo.Size = new System.Drawing.Size(800, 100);
+            this.panelTopo.TabIndex = 0;
+            this.panelTopo.UseWaitCursor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(83, 23);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(224, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro de Clientes";
+            this.lblTitulo.UseWaitCursor = true;
+            // 
+            // tb
+            // 
+            this.tb.Controls.Add(this.tbCadastro);
+            this.tb.Controls.Add(this.tbConsulta);
+            this.tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb.Location = new System.Drawing.Point(12, 106);
+            this.tb.Name = "tb";
+            this.tb.SelectedIndex = 0;
+            this.tb.Size = new System.Drawing.Size(776, 253);
+            this.tb.TabIndex = 1;
+            this.tb.UseWaitCursor = true;
+            // 
+            // tbCadastro
+            // 
+            this.tbCadastro.Controls.Add(this.txtCPF);
+            this.tbCadastro.Controls.Add(this.lblCPF);
+            this.tbCadastro.Controls.Add(this.txtEmail);
+            this.tbCadastro.Controls.Add(this.lblEmail);
+            this.tbCadastro.Controls.Add(this.txtNomeSocial);
+            this.tbCadastro.Controls.Add(this.lblNomeSocial);
+            this.tbCadastro.Controls.Add(this.txtNomeCompleto);
+            this.tbCadastro.Controls.Add(this.lblNomeCompleto);
+            this.tbCadastro.Location = new System.Drawing.Point(4, 29);
+            this.tbCadastro.Name = "tbCadastro";
+            this.tbCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tbCadastro.Size = new System.Drawing.Size(768, 220);
+            this.tbCadastro.TabIndex = 0;
+            this.tbCadastro.Text = "Dados de Cliente";
+            this.tbCadastro.UseVisualStyleBackColor = true;
+            this.tbCadastro.UseWaitCursor = true;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(153, 122);
+            this.txtCPF.Mask = "###,###,###-##";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(123, 26);
+            this.txtCPF.TabIndex = 9;
+            this.txtCPF.UseWaitCursor = true;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblCPF.Location = new System.Drawing.Point(107, 125);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(40, 20);
+            this.lblCPF.TabIndex = 8;
+            this.lblCPF.Text = "CPF";
+            this.lblCPF.UseWaitCursor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(153, 85);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(272, 26);
+            this.txtEmail.TabIndex = 5;
+            this.txtEmail.UseWaitCursor = true;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblEmail.Location = new System.Drawing.Point(94, 88);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "E-mail";
+            this.lblEmail.UseWaitCursor = true;
+            // 
+            // txtNomeSocial
+            // 
+            this.txtNomeSocial.Location = new System.Drawing.Point(153, 48);
+            this.txtNomeSocial.Name = "txtNomeSocial";
+            this.txtNomeSocial.Size = new System.Drawing.Size(272, 26);
+            this.txtNomeSocial.TabIndex = 3;
+            this.txtNomeSocial.UseWaitCursor = true;
+            // 
+            // lblNomeSocial
+            // 
+            this.lblNomeSocial.AutoSize = true;
+            this.lblNomeSocial.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblNomeSocial.Location = new System.Drawing.Point(49, 51);
+            this.lblNomeSocial.Name = "lblNomeSocial";
+            this.lblNomeSocial.Size = new System.Drawing.Size(98, 20);
+            this.lblNomeSocial.TabIndex = 2;
+            this.lblNomeSocial.Text = "Nome Social";
+            this.lblNomeSocial.UseWaitCursor = true;
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(153, 11);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(272, 26);
+            this.txtNomeCompleto.TabIndex = 1;
+            this.txtNomeCompleto.UseWaitCursor = true;
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(24, 14);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(123, 20);
+            this.lblNomeCompleto.TabIndex = 0;
+            this.lblNomeCompleto.Text = "Nome Completo";
+            this.lblNomeCompleto.UseWaitCursor = true;
+            // 
+            // tbConsulta
+            // 
+            this.tbConsulta.Location = new System.Drawing.Point(4, 29);
+            this.tbConsulta.Name = "tbConsulta";
+            this.tbConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tbConsulta.Size = new System.Drawing.Size(768, 299);
+            this.tbConsulta.TabIndex = 1;
+            this.tbConsulta.Text = "Consulta";
+            this.tbConsulta.UseVisualStyleBackColor = true;
+            this.tbConsulta.UseWaitCursor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSalvar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(16, 377);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(147, 61);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // frmCadastrodeClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.panelTopo);
+            this.Name = "frmCadastrodeClientes";
+            this.Text = "Cadastro";
+            this.UseWaitCursor = true;
+            this.panelTopo.ResumeLayout(false);
+            this.panelTopo.PerformLayout();
+            this.tb.ResumeLayout(false);
+            this.tbCadastro.ResumeLayout(false);
+            this.tbCadastro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTopo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TabControl tb;
+        private System.Windows.Forms.TabPage tbCadastro;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNomeSocial;
+        private System.Windows.Forms.Label lblNomeSocial;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.TabPage tbConsulta;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ cadastrodeclientes/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace cadastrodeclientes
+{
+    public partial class frmCadastrodeClientes : Form
+    {
+        public frmCadastrodeClientes()
+        {
+            InitializeComponent();
+        }
+
+        private bool isValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
+        }
+
+        private bool isValidCPFLength(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validação dos campos obrigatórios
+                if (string.IsNullOrEmpty(txtNomeCompleto.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtEmail.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtCPF.Text.Trim()))
+                {
+                    MessageBox.Show("Todos os campos devem ser preenchidos.",
+                                    "Validação",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Validação Email
+                string email = txtEmail.Text.Trim();
+                if (!isValidEmail(email))
+                {
+                    MessageBox.Show("E-mail inválido. Certifique-se de que o e-mail está no formato correto.",
+                                    "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Validação CPF
+                string cpf = txtCPF.Text.Trim();
+                if (!isValidCPFLength(cpf))
+                {
+                    MessageBox.Show("CPF inválido. Certifique-se que o CPF tem 11 dígitos numéricos.",
+                                    "Validação",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Trata outros tipos de erro
+                MessageBox.Show("Ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+    }
+}
