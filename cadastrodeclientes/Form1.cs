38d6de80583bae2dc86fda1dad862c87d4046c19
gustavorssilva3 <gustavo.rssilva3@senacsp.edu.br>
2025-04-04 17:12:38 -0300

    Atualização 04-04

--- cadastrodeclientes/Form1.cs
+++ cadastrodeclientes/Form1.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
 
 namespace cadastrodeclientes
 {
     public partial class frmCadastrodeClientes : Form
     {
+        //Conexão com o banco de dados MySQL
+        MySqlConnection Conexao;
+        string data_source = "datasource=localhost; username=root; password=; database=db_cadastro";
         public frmCadastrodeClientes()
         {
             InitializeComponent();
@@ -68,12 +72,63 @@ namespace cadastrodeclientes
                                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
+
+                //Cria a conexão com o banco de dados
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+                //Teste de abertura
+                // MessageBox.Show("Conexão aberta com sucesso");
+
+                //Comando SQL para inserir um novo cliente no banco de dados
+                MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = Conexao
+                };
+
+                cmd.Prepare();
+
+                cmd.CommandText = "INSERT INTO dadosdecliente(nomecompleto, nomesocial, email, cpf) " +
+                    "VALUES (@nomecompleto, @nomesocial, @email, @cpf)";
+
+                //Adiciona parâmetros com os dados do formulário
+                cmd.Parameters.AddWithValue("@nomecompleto", txtNomeCompleto.Text.Trim());
+                cmd.Parameters.AddWithValue("@nomesocial", txtNomeSocial.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@cpf", cpf);
+
+                // Executa o comando de inserção no bando de dados
+                cmd.ExecuteNonQuery();
+
+                //Menssagem de sucesso
+                MessageBox.Show("Contato inserido com sucesso: ", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
+            catch(MySqlException ex)
+            {
+                //Trata erros relacionados ao SQL
+                MessageBox.Show("Erro " + ex.Number + " ocorreu " + ex.Message, 
+                    "Erro",
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+
             catch (Exception ex)
             {
                 // Trata outros tipos de erro
-                MessageBox.Show("Ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                throw;
+                MessageBox.Show("Ocorreu: " + ex.Message, 
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //Garante que a conexão com banco de dados será fechada mesmo se ocorrer erro
+                if(Conexao != null && Conexao.State == ConnectionState.Open)
+                {
+                    Conexao.Close();
+                    //Texte de Fechamento
+                    // MessageBox.Show("Conexão fechada com sucesso");
+                }
             }
         }
     }
